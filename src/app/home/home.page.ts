1c6d2657432241fb64ad0ab18c334b29300c35ee
zZnexusZz <agekanutpw@gmail.com>
2021-11-01 16:37:32 +0700

    ionic test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public alertController: AlertController) {
+    console.log("test")
+  }
+  async _alert(){
+    console.log("alert");
+    const alert = await this.alertController.create({
+      header : 'This Header',
+      subHeader : 'This Sub Header',
+      message : 'This test message'
+    });
+    await alert.present();
+  }
+    
 
 }
